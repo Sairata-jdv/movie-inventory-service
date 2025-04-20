6a59b22a27f9ef097ea0e1a3a6ffcb249ec4f2cc
sairata <sairata.jdv@gmail.com>
2025-04-20 18:25:55 +0530

    Movie-inventory-service setup my first commit

---
+++ src/main/java/movie_ticketBooking_service/MovieInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package movie_ticketBooking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/movie_ticketBooking_service/MovieInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package movie_ticketBooking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
